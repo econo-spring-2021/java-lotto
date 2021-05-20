a4fca0570395bfd4b8f047e2e6e5fde444d95ef4
Kwon770 <sckwon770@gmail.com>
2021-05-21 02:31:25 +0900

    feat: Add methos to generate user lotto

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,15 +1,17 @@
 package lotto.controller;
 
+import lotto.domain.LottoTicket;
 import lotto.domain.UserAccount;
 import lotto.view.InputView;
 import lotto.view.OutputView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LottoController {
     InputView inputView = new InputView();
     OutputView outputView = new OutputView();
     UserAccount userAccount;
-
-    private static final int LOTTOE_PRICE = 1000;
 
     public void getUserMoney() {
         outputView.askMoney();
@@ -17,11 +19,16 @@ public class LottoController {
     }
 
     public void generateUserLottoTicket() {
-        int lotteCount = getBuyingTickeyCount();
-        outputView.printBuyingLotteCount(lotteCount);
+        int lottoCount = getBuyingTickeyCount();
+        outputView.printBuyingLotteCount(lottoCount);
+
+        List<LottoTicket> tickets = new ArrayList<>();
+        for (int i = 0; i < lottoCount; i++) {
+            tickets.add(new LottoTicket());
+        }
     }
 
     private int getBuyingTickeyCount() {
-        return userAccount.getMoney() / LOTTOE_PRICE;
+        return userAccount.getMoney() / LottoTicket.getLottoPrice();
     }
 }
--- src/main/java/lotto/domain/LottoTicket.java
+++ src/main/java/lotto/domain/LottoTicket.java
@@ -1,11 +1,26 @@
 package lotto.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LottoTicket {
     private final List<Integer> numbers;
 
+    private static final int LOTTOE_PRICE = 1000;
+
     public LottoTicket(List<Integer> numbers) {
         this.numbers = numbers;
+    }
+
+    public LottoTicket() {
+        List<Integer> tempNumbers = new ArrayList<>();
+        for(int i = 0; i < 6; i++) {
+            tempNumbers.add((int) Math.random() * 46);
+        }
+        numbers = tempNumbers;
+    }
+
+    public static int getLottoPrice() {
+        return LOTTOE_PRICE;
     }
 }
