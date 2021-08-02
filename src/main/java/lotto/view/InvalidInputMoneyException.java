53ee3cab17ff5aa016514828a24766e90ea975bf
Hongdonggeon <ghd700@naver.com>
2021-08-02 20:27:12 +0900

    feat : 로또 구매 금액 1000원 미만시 예외처리클래스

--- src/main/java/lotto/view/InvalidInputMoneyException.java
+++
@@ -1,9 +0,0 @@
-package lotto.view;
-
-public class InvalidInputMoneyException extends Exception {
-    private static final String INVALID_INPUT_MONEY_EXCEPTION = "로또는 1000원 이상 부터 살 수 있습니다.";
-
-    public InvalidInputMoneyException(){
-        super(INVALID_INPUT_MONEY_EXCEPTION);
-    }
-}
